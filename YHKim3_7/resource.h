6a3bfa909a08c5641bc8410cfc204df54deace06
kyha97 <kyha97@naver.com>
2019-10-12 00:40:03 +0900

    Add3_7

---
+++ YHKim3_7/resource.h
@@ -0,0 +1,25 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// YHKim37.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_YHKIM3_7_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     129
+#define IDB_BMP                         129
+#define IDC_BUTT_PAINT                  1000
+#define IDC_BUTT_CLEAN                  1001
+#define IDC_BUTT_EXIT                   1002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
