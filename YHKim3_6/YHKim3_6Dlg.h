0adf76c553261db91ab2f97da1863cba43d09516
kyha97 <kyha97@naver.com>
2019-10-12 00:16:59 +0900

    Add 3_6

---
+++ YHKim3_6/YHKim3_6Dlg.h
@@ -0,0 +1,37 @@
+﻿
+// YHKim3_6Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CYHKim36Dlg 대화 상자
+class CYHKim36Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CYHKim36Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_YHKIM3_6_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButtDraw();
+	afx_msg void OnBnClickedButtClean();
+};
