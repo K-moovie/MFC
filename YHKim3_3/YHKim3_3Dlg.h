db3aec59d55ed16b5801a3410c550bbe80f35fed
kyha97 <kyha97@naver.com>
2019-10-11 23:48:33 +0900

    Add 3_3

---
+++ YHKim3_3/YHKim3_3Dlg.h
@@ -0,0 +1,37 @@
+﻿
+// YHKim3_3Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CYHKim33Dlg 대화 상자
+class CYHKim33Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CYHKim33Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_YHKIM3_3_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CScrollBar m_ctlHori;
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+};
