b55df5da02a8d3eb2e4819b945a269d95670e72d
kyha97 <kyha97@naver.com>
2019-10-12 14:40:31 +0900

    Add Test4

---
+++ YHKimTest4/YHKimTest4Dlg.cpp
@@ -0,0 +1,197 @@
+﻿
+// YHKimTest4Dlg.cpp: 구현 파일
+//
+
+#include "stdafx.h"
+#include "YHKimTest4.h"
+#include "YHKimTest4Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CYHKimTest4Dlg 대화 상자
+
+
+
+CYHKimTest4Dlg::CYHKimTest4Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_YHKIMTEST4_DIALOG, pParent)
+	, m_ctlEdit(_T(""))
+	, m_c1(FALSE)
+	, m_c2(FALSE)
+	, m_c3(FALSE)
+	, m_c4(FALSE)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CYHKimTest4Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT_INPUT, m_ctlEdit);
+	DDX_Control(pDX, IDC_LIST, m_ctlList);
+	DDX_Check(pDX, IDC_CHECK1, m_c1);
+	DDX_Check(pDX, IDC_CHECK2, m_c2);
+	DDX_Check(pDX, IDC_CHECK3, m_c3);
+	DDX_Check(pDX, IDC_CHECK4, m_c4);
+}
+
+BEGIN_MESSAGE_MAP(CYHKimTest4Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTT_ADD, &CYHKimTest4Dlg::OnBnClickedButtAdd)
+END_MESSAGE_MAP()
+
+
+// CYHKimTest4Dlg 메시지 처리기
+
+BOOL CYHKimTest4Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CYHKimTest4Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 응용 프로그램의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CYHKimTest4Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CYHKimTest4Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+void CYHKimTest4Dlg::OnBnClickedButtAdd()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData(TRUE);
+	CString str; str = m_ctlEdit + _T(":");
+	CString c1; c1.Format (_T("독서"));
+	CString c2; c2.Format (_T("수영"));
+	CString c3; c3.Format (_T("영화감상"));
+	CString c4; c4.Format (_T("등산"));
+	CString rest; rest.Format(_T(", "));
+
+	if (m_c1 == TRUE && m_c2 == FALSE && m_c3 == FALSE && m_c4 == FALSE)
+		str += c1;
+	if ((m_c1 == TRUE) && (m_c2 == TRUE || m_c3 == TRUE || m_c4 == TRUE))
+		str = str + c1 + rest;
+
+	if (m_c2 == TRUE && m_c3 == FALSE && m_c4 == FALSE)
+		str += c2;
+	if (m_c2 == TRUE && ( m_c3 == TRUE || m_c4 == TRUE))
+		str = str + c2 + rest;
+
+	if (m_c3 == TRUE && m_c4 == FALSE)
+		str += c3;
+	if (m_c3 == TRUE &&  m_c4 == TRUE)
+		str = str + c3 + rest;
+
+	if (m_c4 == TRUE)
+		str += c4;
+	m_ctlList.AddString(str);
+}
---
+++ YHKimTest4/YHKimTest4Dlg.h
@@ -0,0 +1,42 @@
+﻿
+// YHKimTest4Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CYHKimTest4Dlg 대화 상자
+class CYHKimTest4Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CYHKimTest4Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_YHKIMTEST4_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CString m_ctlEdit;
+	CListBox m_ctlList;
+	BOOL m_c1;
+	BOOL m_c2;
+	BOOL m_c3;
+	BOOL m_c4;
+	afx_msg void OnBnClickedButtAdd();
+};
