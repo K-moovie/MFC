c882c066c053ebc287c9e5669c269e601a294eb9
kyha97 <kyha97@naver.com>
2019-10-12 10:16:47 +0900

    Add test4

---
+++ YHKimTest3/YHKimTest3Dlg.h
@@ -0,0 +1,37 @@
+﻿
+// YHKimTest3Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CYHKimTest3Dlg 대화 상자
+class CYHKimTest3Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CYHKimTest3Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_YHKIMTEST3_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CScrollBar m_ctlHori;
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+};
