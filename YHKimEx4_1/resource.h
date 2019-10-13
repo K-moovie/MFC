9a355a1dea39dc4b16e5ef2b831614b3435efd84
kyha97 <kyha97@naver.com>
2019-10-13 21:20:48 +0900

    Add Ex4_1

---
+++ YHKimEx4_1/resource.h
@@ -0,0 +1,28 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// YHKimEx41.rc에서 사용되고 있습니다.
+//
+#define ID_TIMER_A                      0
+#define ID_TIMER_A2                     1
+#define ID_TIMER_B                      1
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_YHKIMEX4_1_DIALOG           102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU                        129
+#define IDD_POPUP                       131
+#define IDC_EDIT_INPUT                  1000
+#define ID_32771                        32771
+#define ID_PAINT                        32772
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
