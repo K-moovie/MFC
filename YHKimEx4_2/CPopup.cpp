f4dba46817c3ba26dbe3781d05f5358294c8703d
kyha97 <kyha97@naver.com>
2019-10-14 00:04:40 +0900

    Add Ex4_2

---
+++ YHKimEx4_2/CPopup.cpp
@@ -0,0 +1,36 @@
+﻿// CPopup.cpp: 구현 파일
+//
+
+#include "stdafx.h"
+#include "YHKimEx4_2.h"
+#include "CPopup.h"
+#include "afxdialogex.h"
+
+
+// CPopup 대화 상자
+
+IMPLEMENT_DYNAMIC(CPopup, CDialog)
+
+CPopup::CPopup(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_DIALOG_POPUP, pParent)
+	, m_input(0)
+{
+
+}
+
+CPopup::~CPopup()
+{
+}
+
+void CPopup::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT_INPUT, m_input);
+}
+
+
+BEGIN_MESSAGE_MAP(CPopup, CDialog)
+END_MESSAGE_MAP()
+
+
+// CPopup 메시지 처리기
