a577cc40a05e5ad0f894cd68d16884aaf25fa797
kyha97 <kyha97@naver.com>
2019-10-13 18:43:25 +0900

    Add 4_3

---
+++ YHKim4_3/CPopup.cpp
@@ -0,0 +1,42 @@
+﻿// CPopup.cpp: 구현 파일
+//
+
+#include "stdafx.h"
+#include "YHKim4_3.h"
+#include "CPopup.h"
+#include "afxdialogex.h"
+
+
+// CPopup 대화 상자
+
+IMPLEMENT_DYNAMIC(CPopup, CDialog)
+
+CPopup::CPopup(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_DIALOG_POPUP, pParent)
+	, m_n1(0)
+	, m_n2(0)
+{
+
+}
+
+CPopup::~CPopup()
+{
+}
+
+void CPopup::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT_N1, m_n1);
+	DDX_Text(pDX, IDC_EDIT_N2, m_n2);
+}
+
+
+BEGIN_MESSAGE_MAP(CPopup, CDialog)
+
+END_MESSAGE_MAP()
+
+
+// CPopup 메시지 처리기
+
+
+
