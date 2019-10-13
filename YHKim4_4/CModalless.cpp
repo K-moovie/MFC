93487930c9ce19c6af6dc7886416f7876de60430
kyha97 <kyha97@naver.com>
2019-10-13 19:29:45 +0900

    Ass 4_4

---
+++ YHKim4_4/CModalless.cpp
@@ -0,0 +1,56 @@
+﻿// CModalless.cpp: 구현 파일
+//
+
+#include "stdafx.h"
+#include "YHKim4_4.h"
+#include "CModalless.h"
+#include "afxdialogex.h"
+
+
+// CModalless 대화 상자
+
+IMPLEMENT_DYNAMIC(CModalless, CDialog)
+
+CModalless::CModalless(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_DIALOG_MODALLESS, pParent)
+{
+
+}
+
+CModalless::~CModalless()
+{
+}
+
+void CModalless::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CModalless, CDialog)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+// CModalless 메시지 처리기
+
+
+void CModalless::OnPaint()
+{
+	CPaintDC dc(this); // device context for painting
+					   // TODO: 여기에 메시지 처리기 코드를 추가합니다.
+					   // 그리기 메시지에 대해서는 CDialog::OnPaint()을(를) 호출하지 마십시오.
+	CPen pEn;
+	if (lineStyle == TRUE)
+	{
+		pEn.CreatePen(PS_DOT, lineWidth, RGB(0, 0, 0));
+	}
+	else
+	{
+		pEn.CreatePen(PS_SOLID, lineWidth, RGB(0, 0, 0));
+	}
+	dc.SelectObject(pEn);
+	dc.MoveTo(0, 100);
+	dc.LineTo(200, 100);
+	pEn.DeleteObject();
+}
---
+++ YHKim4_4/YHKim4_4Dlg.h
@@ -0,0 +1,39 @@
+﻿
+// YHKim4_4Dlg.h: 헤더 파일
+//
+#include "CModalless.h"
+#pragma once
+
+
+// CYHKim44Dlg 대화 상자
+class CYHKim44Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CYHKim44Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+	CModalless *m_pDlg;
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_YHKIM4_4_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	int m_width;
+	BOOL m_style;
+	afx_msg void OnBnClickedRadio1();
+	afx_msg void OnChangeEditWidthx();
+};
