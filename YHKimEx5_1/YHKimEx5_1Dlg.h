2b9d6a42510395d7b959c765ef96230d358e8ff2
kyha97 <kyha97@naver.com>
2019-10-19 17:51:07 +0900

    AddEx5_1

---
+++ YHKimEx5_1/YHKimEx5_1Dlg.h
@@ -0,0 +1,39 @@
+﻿
+// YHKimEx5_1Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CYHKimEx51Dlg 대화 상자
+class CYHKimEx51Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CYHKimEx51Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_YHKIMEX5_1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	CPoint m_arrPoint[255];
+	UINT m_nCount;
+};
