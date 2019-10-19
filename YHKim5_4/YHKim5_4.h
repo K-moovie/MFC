560de3759b4fd3f5084047aa138af62a563c6c13
kyha97 <kyha97@naver.com>
2019-10-19 20:35:47 +0900

    Add Ex05

---
+++ YHKim5_4/YHKim5_4.h
@@ -0,0 +1,32 @@
+﻿
+// YHKim5_4.h: PROJECT_NAME 응용 프로그램에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'stdafx.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CYHKim54App:
+// 이 클래스의 구현에 대해서는 YHKim5_4.cpp을(를) 참조하세요.
+//
+
+class CYHKim54App : public CWinApp
+{
+public:
+	CYHKim54App();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CYHKim54App theApp;
---
+++ YHkimEx5_6/YHkimEx5_6Dlg.h
@@ -0,0 +1,39 @@
+﻿
+// YHkimEx5_6Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CYHkimEx56Dlg 대화 상자
+class CYHkimEx56Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CYHkimEx56Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_YHKIMEX5_6_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	BOOL m_isClicked;
+};
