497e676aec8f7d32e55b55256055ce9484f045d4
kyha97 <kyha97@naver.com>
2019-10-22 21:23:14 +0900

    Add TestEx2

---
+++ YHKimTestEx2/YHKimTestEx2Dlg.h
@@ -0,0 +1,46 @@
+﻿
+// YHKimTestEx2Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CYHKimTestEx2Dlg 대화 상자
+class CYHKimTestEx2Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CYHKimTestEx2Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_YHKIMTESTEX2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	BOOL m_isLD;
+	BOOL m_isLU;
+	BOOL m_isOver;
+	CPoint x[10];
+	CPoint paintS, paintE;
+	UINT m_nCount;
+	int m_x, m_y;
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags);
+};
