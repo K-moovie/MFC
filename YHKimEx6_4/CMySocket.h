f82a099080b2534644e1ed68859436ac30d1eadd
kyha97 <kyha97@naver.com>
2019-11-06 12:19:25 +0900

    Add YHKimEx6_4

---
+++ YHKimEx6_4/CMySocket.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <afxsock.h>
+class CMySocket :
+	public CAsyncSocket
+{
+public:
+	CMySocket();
+	virtual ~CMySocket();
+	void SetParent(CDialog* pWnd);
+private:
+	CDialog *m_pWnd;
+public:
+	virtual void OnAccept(int nErrorCode);
+	virtual void OnConnect(int nErrorCode);
+	virtual void OnReceive(int nErrorCode);
+	virtual void OnClose(int nErrorCode);
+	virtual void OnSend(int nErrorCode);
+};
+
